37b2b4a0411fbadaec79bc7ca6372cf783bf32a0
Nath Dabne <nathaniel.v.faa@gmail.com>
2022-12-01 13:36:17 +0800

    Update Fix Animations

--- components/gallery.jsx
+++ components/gallery.jsx
@@ -19,11 +19,11 @@ const gallery = () => {
   <div ref={galleria} className='flex flex-col max-sm:flex-col max-md:flex-col max-sm:hidden max-md:hidden max-lg:hidden mb-10'>
     <h1 className='text-black text-3xl text-center py-10 font-lobster'>Gallery</h1>
     <div className='flex flex-col justify-center items-center max-h-full'>
-        <img className='w-8/12 max-sm:w-10/12 max-sm:h-full h-[600px] border-2 border-black' src={selectedImage.url} alt="" />
+        <img className='w-auto h-[500px] object-scale-down border-2 border-black' src={selectedImage.url} alt="" />
         <div className='flex flex-wrap flex-row justify-center items-center '>
             {Images.map((img, index) => (
                 <img 
-                 className={'mt-5 my-1 mx-1 w-[100px] h-[100px] cursor-pointer ' + (selectedImage === img ? 'scale-110 border-2 border-black': "")}
+                 className={'mt-5 my-1 mx-1 w-[100px] h-[100px] cursor-pointer object-cover transition-all ' + (selectedImage === img ? 'scale-110 border-2 border-black': "")}
                  src={img.url} 
                  key={index} 
                  onClick={() => setselectedImage(img)} 
--- components/manualservice.jsx
+++ components/manualservice.jsx
@@ -31,13 +31,11 @@ const manualservice = ({Title}) => {
   </div>
     <div id='services' className='font-custom flex flex-row text-center justify-center items-center gap-10 max-sm:flex-col max-md:flex-col max-lg:flex-col'>
       <div className=' flex flex-col w-4/12 items-center justify-start max-sm:w-10/12 max-md:w-10/12 max-lg:w-10/12'>
-        <h1 ref={titleAbove} className=' font-lobster font-bold py-5 text-3xl max-sm:text-2xl'>{Title}</h1>
+        <h1 ref={titleAbove} className=' font-lobster py-5 text-3xl max-sm:text-2xl'>{Title}</h1>
         <img ref={picLeft} className='px-3 max-sm:w-full max-sm:h-full max-md:h-full max-md:w-full max-lg:h-full max-lg:w-full ' src="/images/images7.jpg" alt="" />
       </div>
       <div ref={description} className=' flex flex-col justify-start items-start w-5/12 max-sm:w-10/12 max-sm: gap-5'>
-        <p className=' text-justify'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo consectetur illum odit harum atque maxime incidunt eligendi nisi repudiandae, aliquid unde dicta esse cupiditate at commodi iure perspiciatis repellendus ex.</p>
-        <p className=' text-justify'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo consectetur illum odit harum atque maxime incidunt eligendi nisi repudiandae, aliquid unde dicta esse cupiditate at commodi iure perspiciatis repellendus ex.</p>
-        <p className=' text-justify'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo consectetur illum odit harum atque maxime incidunt eligendi nisi repudiandae, aliquid unde dicta esse cupiditate at commodi iure perspiciatis repellendus ex.</p>
+        <p className=' text-justify'>You could be wondering how to construct a septic system and asking the following questions whether you're building a new home or have an outdated septic system that needs to be replaced. Do I need a professional to install a normal septic tank, or can I do it myself? Does the health department or state regulations allow me to set up my own septic system? What are the most typical tank types? These are legitimate concerns that might make homeowners feel overawed when dealing with the installation of a new septic tank. We're here to help you understand exactly what you need to get a new septic tank system built that may need replacing and to help lessen your stress.</p>
       </div>  
     </div>
     </>
