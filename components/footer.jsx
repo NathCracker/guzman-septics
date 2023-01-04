265464b6f48f88ea3bf192840e2ce15c97625e15
Nath Dabne <nathaniel.v.faa@gmail.com>
2023-01-04 15:27:57 +0800

    Update

--- components/footer.jsx
+++ components/footer.jsx
@@ -4,14 +4,14 @@ import ScrollToTop from 'react-scroll-to-top'
 
 const footer = () => {
   return (
-<div className='flex flex-col bg-slate-800 text-center'>
+<div className='flex flex-col bg-slate-800 text-center' lang='en'>
   <div className='flex justify-around items-center bg-slate-800 min-w-full w-full pt-16 pb-3 max-sm:flex-col max-sm:w-full'>
     <div className='flex flex-col justify-center gap-5 items-center w-2/6'>
       <div className=' flex justify-center items-center text-center w-full max-sm:w-full'>
         <h1 className='text-white text-2xl font-lobster'>Sign up to our Newsletter</h1>
       </div>
       <div className='flex justify-center items-center w-full max-sm:w-full max-sm:pb-5'>
-        <input className='max-sm:w-full appearance-none block py-[5px] w-full focus:outline focus:border border-teal-500' type="text" name="" id="" />
+        <input aria-label='Email Address' className='max-sm:w-full appearance-none block py-[5px] w-full focus:outline focus:border border-teal-500' type="text" name="" id="" />
         <button className=' max-sm:text-xs max-sm:py-2 appearance-none block py-1 border-2   border-slate-900 rounded-sm w-1/2 text-white bg-slate-900 transition-all hover:bg-transparent duration-100  max-sm:w-1/2' type="submit">Sign up</button>
       </div>
     </div>
