fa4e066914ce45b38eb1a00f625cc9f13e7d3680
Nath Dabne <nathaniel.v.faa@gmail.com>
2023-01-04 14:22:26 +0800

    Meta

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,14 +2,30 @@ import React from 'react'
 import Navbar from './navbar'
 import Hotline from './hotline'
 import Footer from './footer'
-const Layout = ({ children }) => {
+const Layout = ({ children, pageMeta }) => {
+
+  const meta = {
+    title: 'The Best Septice Service in Houston',
+    description: 'Your Trusted Septic Partner',
+    type: 'website',
+    ...pageMeta
+  };
+
   return (
+    <>
+    <Head>
+      <title>{meta.title}</title>
+      <meta name='description' content={meta.description} />
+      <meta property='type' content={meta.type} />
+    </Head>
+    
     <div className='scroll-smooth'>
         <Hotline/>
         <Navbar/>
         { children }
         <Footer/>
     </div>
+    </>
   )
 }
 
